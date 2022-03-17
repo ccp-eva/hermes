b9c8cc384b78e131fb8867ebda99a4dd76d5c49d
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-03-17 18:33:28 +0100

    remove spinner from backup

--- backup.js
+++ backup.js
@@ -1,15 +1,12 @@
 import 'dotenv/config';
 import Rsync from 'rsync';
 import { createDateDirectory } from './createDateDir.js';
-import { createSpinner } from 'nanospinner';
 
 export function backup() {
   const rsync = new Rsync();
   rsync.flags('avzP');
   rsync.source(process.env.SOURCE_DIR);
   rsync.destination(createDateDirectory());
-
-  const spinner = createSpinner();
 
   return new Promise((resolve, reject) => {
     try {
