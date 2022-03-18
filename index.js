c3f1094707464a318ec088aec64a7240253f78dd
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-03-18 06:22:19 +0100

    Get rid of log

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ console.clear();
 // iso 8601 date string without time (YYYY-MM-DD)
 global.today = new Date().toISOString().substring(0, 10);
 
-const log = console.log;
-log(chalk.blue.bold('CCP Backup & Video Compression'));
+console.log(chalk.blue.bold('CCP Backup & Video Compression') + ` — ${today}`);
 
 async function question1() {
   const answer = await inquirer.prompt({
