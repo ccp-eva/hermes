8991f6e83388e932340ae943f459eb512af9ae64
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-03-20 11:02:55 +0100

    refactor directory walk

---
+++ walkSync.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+export function* walkSync(dir) {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* walkSync(path.join(dir, file.name));
+    } else {
+      yield path.join(dir, file.name);
+    }
+  }
+}
