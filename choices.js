68ea0638432c8afb100dc3c4d6404afb088f464e
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-03-20 19:48:38 +0100

    Add empty source & match wording

--- choices.js
+++ choices.js
@@ -2,13 +2,19 @@ export const choices = {
   main: [
     'Backup, Clean Source Directory, Video Compression',
     '  Single Steps',
-    'Parity Check (Target and Cloud)',
+    'Parity check (Target ⇆ Cloud)',
     'Exit',
   ],
   single: [
     'Backup SD card to external drive',
     'Clean source directory (removes *BPHH* folders on SD card)',
-    'Parity check & video compression (compress videos which are not present in cloud folder)',
+    'Parity check (Target ⇆ Cloud)',
+    'Video compression (compress videos which are not present in cloud folder)',
+    'Exit',
+  ],
+  emptySource: [
+    'Parity check (Target ⇆ Cloud)',
+    'Video compression (compress videos which are not present in cloud folder)',
     'Exit',
   ],
 };
